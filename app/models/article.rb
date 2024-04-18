1118055ae9517027066cacc3f597ac76a893a94e
Rômulo Augusto <romuloffall@gmail.com>
2024-04-18 14:14:19 -0300

    #add visibility concern

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,14 +1,8 @@
 class Article < ApplicationRecord
+  include Visible
+
   has_many :comments, dependent: :destroy
 
-  VALID_STATUSES = %w[public private archived].freeze
-
-  validates :status, inclusion: { in: VALID_STATUSES }
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10, maximum: 300 }
-
-  def archived?
-    status == 'archived'
-  end
 end
-e o
