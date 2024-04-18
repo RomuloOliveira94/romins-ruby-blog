803576d5f67daefc272fce43078fa6278e8f2f19
Rômulo Augusto <romuloffall@gmail.com>
2024-04-18 13:16:06 -0300

    #add status on comments and articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,14 @@
 class Article < ApplicationRecord
   has_many :comments, dependent: :destroy
 
+  VALID_STATUSES = %w[public private archived].freeze
+
+  validates :status, inclusion: { in: VALID_STATUSES }
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10, maximum: 300 }
+
+  def archived?
+    status == 'archived'
+  end
 end
+e o
