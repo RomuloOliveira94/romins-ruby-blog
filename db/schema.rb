4c5ed4148812220b049d5b43601c18c2c73427ff
Rômulo Augusto <romuloffall@gmail.com>
2024-04-20 11:58:42 -0300

    #remove comment status

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_18_160426) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_20_143954) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -25,7 +25,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_18_160426) do
     t.integer "article_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "status"
     t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
